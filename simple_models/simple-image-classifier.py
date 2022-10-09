528c83101120574e89430ba4d42da81dc8a5889f
Victor Mitchell <victorcmitchell@gmail.com>
2022-10-09 09:52:43 +0900

    added logs and tensorboard

--- simple_models/simple-image-classifier.py
+++ simple_models/simple-image-classifier.py
@@ -1,8 +1,18 @@
+#!/usr/bin/env python3
 
+from gc import callbacks
+import os
 import matplotlib as plt
 import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
+
+root_logdir = os.path.join(os.curdir, "logs")
+
+def get_run_logdir():
+    import time
+    run_id = time.strftime("run_%Y_%m_%d-%H_%M_%S")
+    return os.path.join(root_logdir, run_id)
 
 fashion_mnist = keras.datasets.fashion_mnist
 (x_train_full, y_train_full), (x_test, y_test) = fashion_mnist.load_data()
@@ -21,4 +31,10 @@ model.add(keras.layers.Dense(10, activation="softmax"))
 
 model.compile(loss="sparse_categorical_crossentropy", optimizer="sgd", metrics=["accuracy"])
 
-history = model.fit(x_train, y_train, epochs=30, validation_data=(x_valid, y_valid))
+run_logdir = get_run_logdir()
+tensorboard_cb = keras.callbacks.TensorBoard(run_logdir) # connect to http://localhost:6006
+
+history = model.fit(x_train, y_train, epochs=30, validation_data=(x_valid, y_valid),
+                    callbacks=[tensorboard_cb])
+
+model.save("fashion_classifier.h5")
