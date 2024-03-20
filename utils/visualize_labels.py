3bf658cf7125beeabcdd87ddb4b1f339489e0c29
Victor <victorcmitchell@gmail.com>
2024-03-20 17:42:41 +0900

    visualization of label bboxes

--- utils/visualize_labels.py
+++ utils/visualize_labels.py
@@ -13,7 +13,6 @@ def draw_object_labels(save_path, img, classes, bboxs, polygons=None):
     cv_img = np.transpose(cv_img, (1,2,0))
     cv_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
     h, w, _ = cv_img.shape
-    print("img shape",cv_img.shape)
 
     np_classes = classes.numpy()
     np_bboxs = bboxs.numpy()
@@ -23,11 +22,9 @@ def draw_object_labels(save_path, img, classes, bboxs, polygons=None):
         np_polygons = polygons.numpy()
 
     for cls, bbox, poly in zip(np_classes, np_bboxs, np_polygons):
-        top_left = (int((bbox[0] - bbox[2]/2) * w), int((bbox[1] + bbox[3]/2) * h))
-        bottom_right = (int((bbox[0] + bbox[2]/2) * w), int((bbox[1] - bbox[3]/2) * h))
-        # cv2.rectangle(cv_img, top_left, bottom_right, (255, 0, 0), 1)
-        print(cls,bbox[0],bbox[1])
-        cv2.circle(cv_img, (int(bbox[0] * w), int(bbox[1] * h)), 2, (0, 0, 255), -1)
+        top_left = (int((bbox[0] - bbox[2]/2) * w), int((bbox[1] - bbox[3]/2) * h))
+        bottom_right = (int((bbox[0] + bbox[2]/2) * w), int((bbox[1] + bbox[3]/2) * h))
+        cv2.rectangle(cv_img, top_left, bottom_right, (255, 0, 0), 1)
     
     print("Saving", save_path)
     cv2.imwrite(save_path, cv_img)
