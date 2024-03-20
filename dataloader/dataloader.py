6c4a87d274dc5c71e1ed3ce6977dc8e929b36bd1
Victor <victorcmitchell@gmail.com>
2024-03-20 16:35:21 +0900

    sort image and labels files

--- dataloader/dataloader.py
+++ dataloader/dataloader.py
@@ -1,9 +1,10 @@
 import os
 import argparse
-import numpy as np
+import uuid
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision.io import read_image
+from utils.visualize_labels import draw_object_labels
 
 class YoloDataset(Dataset):
     def __init__(self, dataset_path, test = False):
@@ -11,8 +12,8 @@ class YoloDataset(Dataset):
         self.images_path = os.path.join(dataset_path, "images/")
         self.labels_path = os.path.join(dataset_path, "labels/")
 
-        self.image_paths = [os.path.join(self.images_path, x) for x in os.listdir(self.images_path)]
-        self.label_paths = [os.path.join(self.labels_path, x) for x in os.listdir(self.labels_path)]
+        self.image_paths = [os.path.join(self.images_path, x) for x in sorted(os.listdir(self.images_path))]
+        self.label_paths = [os.path.join(self.labels_path, x) for x in sorted(os.listdir(self.labels_path))]
     
     def loadLabels(self, labelsPath):
         with open(labelsPath) as stream:
@@ -32,21 +33,30 @@ class YoloDataset(Dataset):
         return len(self.image_paths)
 
     def __getitem__(self, index):
-        image = read_image(self.image_paths[index])
-        labels = self.loadLabels(self.label_paths[index])
+        image_path = self.image_paths[index]
+        label_path = self.label_paths[index]
+        print(image_path,"-",label_path)
+        image = read_image(image_path)
+        labels = self.loadLabels(label_path)
         return (image, labels)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Splitting coco labels into yolo format.")
     parser.add_argument('-d', '--dataset', required=True)
+    parser.add_argument('-v', '--visualize', type=bool, action=argparse.BooleanOptionalAction, default=False)
     args = parser.parse_args()
     dataset = YoloDataset(args.dataset)
     dataloader = DataLoader(dataset)
+    if args.visualize:
+        os.makedirs("results/", exist_ok=True)
     for i, data in enumerate(dataloader):
         image, labels = data
         classes, bboxs = labels
-        print("Data", i, "image", image.shape, "classes", classes.shape, "bboxs", bboxs.shape)
+        if args.visualize:
+            save_path = os.path.join("results", uuid.uuid4().hex + ".jpg")
+            draw_object_labels(save_path, image[0], classes[0], bboxs[0])
+
         if i == 9:
             break
     print("Dataloader Length", len(dataloader))
---
+++ utils/visualize_labels.py
@@ -0,0 +1,37 @@
+import torch
+import cv2
+import numpy as np
+
+COLORS = {
+    "red": (255, 0, 0),
+    "green": (0, 255, 0),
+    "blue": (0, 0, 255)
+}
+
+def draw_object_labels(save_path, img, classes, bboxs, polygons=None):
+    cv_img = img.numpy()
+    cv_img = np.transpose(cv_img, (1,2,0))
+    cv_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+    h, w, _ = cv_img.shape
+    print("img shape",cv_img.shape)
+
+    np_classes = classes.numpy()
+    np_bboxs = bboxs.numpy()
+    if polygons is None:
+        np_polygons = np.zeros(np_classes.shape[0])
+    else:
+        np_polygons = polygons.numpy()
+
+    for cls, bbox, poly in zip(np_classes, np_bboxs, np_polygons):
+        top_left = (int((bbox[0] - bbox[2]/2) * w), int((bbox[1] + bbox[3]/2) * h))
+        bottom_right = (int((bbox[0] + bbox[2]/2) * w), int((bbox[1] - bbox[3]/2) * h))
+        # cv2.rectangle(cv_img, top_left, bottom_right, (255, 0, 0), 1)
+        print(cls,bbox[0],bbox[1])
+        cv2.circle(cv_img, (int(bbox[0] * w), int(bbox[1] * h)), 2, (0, 0, 255), -1)
+    
+    print("Saving", save_path)
+    cv2.imwrite(save_path, cv_img)
+
+
+if __name__ == "__main__":
+    pass
