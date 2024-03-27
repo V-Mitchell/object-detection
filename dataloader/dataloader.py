d13092c7908497c79b21388e845a37bf96df6f0c
Victor <victorcmitchell@gmail.com>
2024-03-27 20:47:13 +0900

    dataloader loads label masks

--- dataloader/dataloader.py
+++ dataloader/dataloader.py
@@ -4,7 +4,20 @@ import uuid
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision.io import read_image
+import numpy as np
+import cv2
 from utils.visualize_labels import draw_object_labels
+
+def xyxy2Mask(xyxy, width, height):
+    wh = [width, height]
+    xyxy_points = []
+    for i, x in enumerate(xyxy):
+        xyxy_points.append(int(x * wh[i%2]))
+    points = np.array(xyxy_points).reshape((-1, 2))
+    mask = np.zeros((width,height, 1), np.uint8)
+    cv2.fillConvexPoly(mask, points, (255))
+    return mask
+
 
 class YoloDataset(Dataset):
     def __init__(self, dataset_path, test = False):
@@ -15,33 +28,37 @@ class YoloDataset(Dataset):
         self.image_paths = [os.path.join(self.images_path, x) for x in sorted(os.listdir(self.images_path))]
         self.label_paths = [os.path.join(self.labels_path, x) for x in sorted(os.listdir(self.labels_path))]
     
-    def loadLabels(self, labelsPath):
+    def loadData(self, imagePath, labelsPath):
+        image = read_image(imagePath)
+        _, width, height = image.shape
         with open(labelsPath) as stream:
              object_labels = stream.readlines()
         
         object_classes = []
         object_bboxs = []
+        object_mask = []
         for object_label in object_labels:
-            label_split = object_label.split(' ')
+            label_split = object_label.strip().split(' ')
             object_classes.append(int(label_split[0]))
             x, y, w, h = map(float, label_split[1:5])
             object_bboxs.append([x, y, w, h])
-        return (torch.Tensor(object_classes).int(), torch.Tensor(object_bboxs).float())
+            object_mask.append(xyxy2Mask([x for x in map(float, label_split[5:])], width, height))
+        labels =  (torch.Tensor(object_classes).int(), torch.Tensor(object_bboxs).float(), torch.Tensor(np.array(object_mask)).float())
+        return (image, labels)
 
 
     def __len__(self):
         return len(self.image_paths)
 
     def __getitem__(self, index):
-        image = read_image(self.image_paths[index])
-        labels = self.loadLabels(self.label_paths[index])
-        return (image, labels)
+        return self.loadData(self.image_paths[index], self.label_paths[index])
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Testing dataloaders")
     parser.add_argument('-d', '--dataset', required=True)
     parser.add_argument('-v', '--visualize', type=bool, action=argparse.BooleanOptionalAction, default=False)
+    parser.add_argument('-n', '--num_labels', default=10)
     args = parser.parse_args()
     dataset = YoloDataset(args.dataset)
     dataloader = DataLoader(dataset)
@@ -49,10 +66,11 @@ if __name__ == "__main__":
         os.makedirs("results/", exist_ok=True)
     for i, data in enumerate(dataloader):
         image, labels = data
-        classes, bboxs = labels
+        classes, bboxs, masks = labels
+        print(classes.shape, bboxs.shape, masks.shape)
         if args.visualize:
             save_path = os.path.join("results", uuid.uuid4().hex + ".jpg")
-            draw_object_labels(save_path, image[0], classes[0], bboxs[0])
+            draw_object_labels(save_path, image[0], classes[0], bboxs[0], masks[0])
 
-        if i == 9:
+        if i == args.num_labels:
             break
